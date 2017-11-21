80b5de4aaee1a85bcb5381ff3510016912547987
mcnixon96 <33457533+mcnixon96@users.noreply.github.com>
2017-11-21 13:27:46 +0000

    Add files via upload

---
+++ first_exercise.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.modeling import functional_models
+import random
+
+class RightAscension:
+    def __init__(self,angle):
+        self.hours=int(angle/(np.pi/12))
+        self.minutes=int((angle-self.hours*(np.pi/12))/(np.pi/720))
+        self.seconds=(angle-self.hours*(np.pi/12)-self.minutes*(np.pi/720))/(np.pi/43200)
+
+class Declination:
+    def __init__(self,angle):
+        self.degrees=int(angle/(np.pi/180))
+        self.minutes=int((angle-self.degrees*(np.pi/180))/(np.pi/(180*60)))
+        self.seconds=(angle-self.degrees*(np.pi/180)-self.minutes*(np.pi/(180*60)))/(np.pi/(180*60*60))
+
+        
+def disk_distance_plot(band_no):
+
+    if band_no==3:
+        T_sys=67 #Band 3 parameters
+        freq_lower=8.4*10**10
+        freq_upper=1.16*10**11
+        aperture_eff=0.69
+        ref_flux=2.6*10**-28 #band 3 flux for HD 100546 in Wm-2Hz-1 from SED
+    elif band_no==7:
+        T_sys=251
+        freq_lower=2.75*10**11
+        freq_upper=3.7*10**11
+        aperture_eff=0.74
+        ref_flux=8*10**-24 #band 7 flux for HD 100546 in Wm-2Hz-1 from SED
+
+    obs_time=3600 #observing time in seconds
+
+    sigma=T_sys/((freq_upper-freq_lower)*obs_time)**0.5 #rms noise
+
+    ref_temp=aperture_eff*12**2*ref_flux*10**26/3514.0
+
+    ref_snr=ref_temp/sigma
+
+    distance=np.logspace(2,6,50)
+    snr=np.zeros(len(distance))
+
+    for d in range(len(distance)):
+        snr[d]=ref_snr*10**4/float(distance[d])**2
+
+    plt.plot(distance,snr)
+    plt.show()
+    plt.ylim(0,10)
+    plt.xscale('log')
+    plt.xlabel('distance / pc')
+    plt.ylabel('signal to noise ratio / sigma')
+
+def disk_contour(distance,centre_ra=0,centre_dec=0):
+    
+    distance=float(distance)
+    ra=np.arange(centre_ra-(8.73*10**-8*100),centre_ra+(8.73*10**-8*100),8.73*10**-8)
+    dec=np.arange(centre_dec-(8.73*10**-8*100),centre_dec+(8.73*10**-8*100),8.73*10**-8)
+
+    xx,yy=np.meshgrid(ra,dec)
+
+    peak_flux=800*10**4/float(distance**2) #peak flux in mJy as function of distance (using band 7 here)
+
+    fwhm=50. #in au
+    fwhm_angle=abs(np.arctan((fwhm*4.848*10**-6)/distance))
+    disk_ra=ra[int(len(ra)/2)]
+    disk_dec=dec[int(len(dec)/2)]
+    
+    G=functional_models.Gaussian2D(amplitude=peak_flux,x_mean=disk_ra,y_mean=disk_dec,x_stddev=fwhm_angle/2.355,y_stddev=fwhm_angle/2.355)
+
+    #Creating map of Extragalactic Background Light
+
+    flux_bin_limits=[0.015,0.027,0.047,0.084,0.15,0.267,0.474,0.843,1.124]
+    log_bin_count=np.array([5.5,5.6,5.3,5,5.1,4.9,4.7,4.2,3.4])
+    bin_count=10**log_bin_count
+    flux_bins=np.zeros((len(flux_bin_limits)-1,10))
+    for i in range(len(flux_bin_limits)-1):
+        flux_bins[i,:]=np.logspace(np.log10(flux_bin_limits[i]),np.log10(flux_bin_limits[i+1]),10)
+
+    area=(np.amax(ra)-np.amin(ra))*(np.amax(dec)-np.amin(dec))
+    fractional_area=area/(3.046*10**-4)
+    adjusted_bin_count=fractional_area*bin_count
+    
+    mean_counts=np.random.poisson(adjusted_bin_count)
+
+    for i in range(len(mean_counts)):
+        if mean_counts[i]!=0:
+            for j in range(mean_counts[i]):
+                source_ra=random.choice(ra)
+                source_dec=random.choice(dec)
+                source_flux=random.choice(flux_bins[i,:])
+                source=functional_models.Gaussian2D(amplitude=peak_flux,x_mean=source_ra,y_mean=source_dec,x_stddev=(8.73*10**-8)/2.355,y_stddev=(8.73*10**-8)/2.355)
+                G=G+source
+    
+    levels=np.linspace(0,peak_flux,20)
+
+    z=G(xx,yy)
+    plt.contourf(ra,dec,z,levels)
+    plt.xlabel('Right ascension')
+    plt.ylabel('Declination')
+    plt.colorbar()
+    plt.show()
+
+    return peak_flux
